ddade6a6d46805eb8bb34adfc5685ecd3eeeece7
Murilo-Luciano <murilo.luciano123@gmail.com>
2023-01-28 17:11:06 -0400

    initial css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,40 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-export default function Home() {}
+export default function Home() {
+  return (
+    <>
+      <div className="header">Password Generator</div>
+      <div className="contentContainer">
+        <div className="passwordDisplayContainer">
+          <div className="passwordTextField">
+            {"kjsa$jimd(i&"}
+            <div>icon</div>
+          </div>
+          <div className="copyButton">COPY</div>
+        </div>
+        <div className="passwordOptionsContainer">
+          <div className="slider"></div>
+          <div className="checkboxContainer">
+            <div>
+              <input type={"checkbox"} />
+              <label>Number</label>
+            </div>
+            <div>
+              <input type={"checkbox"} />
+              <label>Symbol</label>
+            </div>
+            <div>
+              <input type={"checkbox"} />
+              <label>Lowercase</label>
+            </div>
+            <div>
+              <input type={"checkbox"} />
+              <label>Uppercase</label>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="footer">Made by Murilo</div>
+    </>
+  );
+}
