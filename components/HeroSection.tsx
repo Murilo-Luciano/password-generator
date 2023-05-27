a773e6b447d3ccf51b50841e1c139ebeca120a6c
Murilo-Luciano <murilo.luciano123@gmail.com>
2023-05-27 19:42:13 -0400

    hero-section

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -2,23 +2,9 @@ import colors from "../styles/colors";
 
 export default () => {
   return (
-    <div style={{ margin: "0 auto", width: 887, marginBottom: 184 }}>
-      <h1
-        style={{
-          fontWeight: 800,
-          fontSize: 52,
-          color: colors.yellow,
-        }}
-      >
-        Get Strong Passwords in Seconds
-      </h1>
-      <p
-        style={{
-          fontWeight: 400,
-          fontSize: 24,
-          color: colors.yellow,
-        }}
-      >
+    <div className="hero-section">
+      <h1>Get Strong Passwords in Seconds</h1>
+      <p>
         Never struggle to come up with a strong password again
         <br />
         Our password generator takes the guesswork out of security
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import "../styles/Home.css";
+import "../styles/Staging.css";
 
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
