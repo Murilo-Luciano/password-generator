fdf0497e7c6f52423c4c7117bfdaf5263a17852f
Murilo-Luciano <murilo.luciano123@gmail.com>
2023-05-28 17:08:54 -0400

    initial-button

--- components/InitialButton.tsx
+++ components/InitialButton.tsx
@@ -1,22 +1,3 @@
-import colors from "../styles/colors";
-
 export default () => {
-  return (
-    <div
-      style={{
-        margin: "0 auto",
-        backgroundColor: colors.veryWeak,
-        borderRadius: 16,
-        width: 337,
-        paddingTop: 6,
-        paddingBottom: 6,
-        fontWeight: 400,
-        fontSize: 32,
-        textAlign: "center",
-        marginBottom: 98,
-      }}
-    >
-      Generate Password
-    </div>
-  );
+  return <div className="initial-button">Generate Password</div>;
 };
